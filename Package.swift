483323788be574c4088f1aaadf918e0ff49d1102
Jean-Francois Duval <jf@mission.dev>
2023-10-22 15:48:55 -0400

    WIP

--- Package.swift
+++ Package.swift
@@ -14,20 +14,14 @@ let package = Package(
             targets: ["BetSwapKit"]
         ),
     ],
-    // dependencies: [
-    //     .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView", .upToNextMajor(from: "4.0.0")),
-    //     .package(url: "https://github.com/marlimox/AloeStackView", .upToNextMajor(from: "1.0.0"))
-    // ],
     targets: [
         .target(
             name: "BetSwapKit",
-            linkerSettings: [
-                .linkedFramework("Foundation")
-            ],
-            // dependencies: [
-            //     "NVActivityIndicatorView",
-            //     "AloeStackView"
-            // ]
+            path: ""
+        ),
+        .binaryTarget(
+            name: "BetSwapKitPackage", 
+            path: "BetSwapKit.xcframework"
         )
     ]
 )
